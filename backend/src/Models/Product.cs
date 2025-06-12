a1b1eb84347124ce90f7d08478801433b3505e61
Andreas Böhm <134922046+LordofGhost@users.noreply.github.com>
2025-06-12 17:29:52 +0000

    Expand product information

--- backend/src/Models/Product.cs
+++ backend/src/Models/Product.cs
@@ -1,7 +1,16 @@
+namespace Supermarket.Models;
+
 public class Product
 {
     // Id can be Null because it is a auto incremnt value in the db
     public int? Id { get; set; }
     required public string Name { get; set; }
-    required public Int32 Price { get; set; }
+    public string? Description { get; set; }
+    required public int CategoryId { get; set; }
+    required public int InStock { get; set; }
+    required public int OnTheShelf { get; set; }
+    required public int Sold { get; set; }
+    required public decimal Price { get; set; }
+    required public decimal TaxRate { get; set; }
+    required public bool IsActive { get; set; }
 }
