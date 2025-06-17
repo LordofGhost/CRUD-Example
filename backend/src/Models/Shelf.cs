902639fd411a3e765330440aac735fec03bc5f94
Andreas Böhm <134922046+LordofGhost@users.noreply.github.com>
2025-06-17 11:19:52 +0000

    Rename Shelf class

--- backend/src/Models/Shelf.cs
+++ backend/src/Models/Shelf.cs
@@ -2,10 +2,10 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Supermarket.Models;
 
-public class Shelve
+public class Shelf
 {
     [Key]
-    public int Id { get; set; }
+    public int ShelfId { get; set; }
     required public Category Category { get; set; }
-    public ICollection<Product>? Products { get; set; }
+    public virtual ICollection<Product>? Products { get; set; }
 }
