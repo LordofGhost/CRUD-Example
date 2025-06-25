fc767021af74ea7c044e73f273cb40b76d7d4c23
Andreas Böhm <134922046+LordofGhost@users.noreply.github.com>
2025-06-25 06:17:57 +0000

    Add load function to Header to get the username

--- frontend/src/components/layout/Header.jsx
+++ frontend/src/components/layout/Header.jsx
@@ -1,6 +1,32 @@
-import { logout } from "../../services/Employees";
+import { useLocation, useNavigate } from "react-router-dom";
+import { logout, getMyName } from "../../services/Employees";
+import { useEffect, useState } from "react";
 
-function Header({ PageName, EmployeeName }) {
+function Header({ PageName }) {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const currentPath = location.pathname;
+  const [employeeName, setEmployeeName] = useState("");
+
+  useEffect(() => {
+    getMyName().then((name) => {
+      if (name == null) {
+        if (currentPath !== "/") {
+          navigate("/");
+        } else {
+          setEmployeeName("");
+        }
+      } else {
+        setEmployeeName(name);
+      }
+    });
+  }, [currentPath, navigate]);
+
+  function handleLogout() {
+    logout();
+    navigate("/");
+  }
+
   return (
     <div className="flex flex-row shadow-lg justify-between">
       <div className="flex flex-row min-w-60 m-2">
@@ -13,11 +39,11 @@ function Header({ PageName, EmployeeName }) {
         {PageName}
       </div>
       <div className="flex flex-row min-w-60 m-2 my-auto justify-end">
-        <div className="font-semibold text-base mx-2">{EmployeeName}</div>
+        <div className="font-semibold text-base mx-2">{employeeName}</div>
         <img
           className="h-6 bg-red-300 hover:bg-red-400 p-1 rounded-md transition-all ease-in-out duration-300"
           src="logout.png"
-          onClick={logout}
+          onClick={handleLogout}
         />
       </div>
     </div>
