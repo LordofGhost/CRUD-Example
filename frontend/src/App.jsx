2d0d0d03ffb4a1bb4a8231505bce71025e6ce519
Andreas Böhm <134922046+LordofGhost@users.noreply.github.com>
2025-06-25 12:12:54 +0000

    Create App router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,21 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Administration from "./pages/Administration";
+import CashRegister from "./pages/CashRegister";
+import Login from "./pages/Login";
+import Warehouse from "./pages/Warehouse";
 
 function App() {
-
   return (
-    <>
-        <h1>Hello</h1>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route />
+        <Route path="/administration" element={<Administration />} />
+        <Route path="/cashregister" element={<CashRegister />} />
+        <Route path="/warehouse" element={<Warehouse />} />
+        <Route path="*" element={<Login />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
