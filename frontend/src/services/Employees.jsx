d26bf207863b302677724262c1702306de6eba17
Andreas Böhm <134922046+LordofGhost@users.noreply.github.com>
2025-06-26 12:39:09 +0000

    Finish Employees service

--- frontend/src/services/Employees.jsx
+++ frontend/src/services/Employees.jsx
@@ -1,32 +1,68 @@
-export const login = async (email, password) => {
+export const getAllEmployees = async () => {
   try {
-    const response = await fetch("/api/Employees/login", {
-      method: "POST",
+    const response = await fetch(`api/Employees`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    return await response.json();
+  } catch (error) {
+    console.error("Could not get Employee: " + error);
+    return null;
+  }
+};
+
+export const getEmployee = async (email) => {
+  try {
+    const response = await fetch(`api/Employees/${email}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    return await response.json();
+  } catch (error) {
+    console.error("Could not get Employee: " + error);
+    return null;
+  }
+};
+
+export const patchEmployee = async (email, firstName, lastName, role) => {
+  try {
+    const response = await fetch(`api/Employees/${email}`, {
+      method: "PATCH",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        email: email,
-        passwordHash: password,
+        firstName: firstName,
+        lastName: lastName,
+        email: " ",
+        role: role,
       }),
     });
     return response.ok;
   } catch (error) {
-    console.error("User could not be logged in: " + error);
+    console.error("User could not be edited: " + error);
     return false;
   }
 };
 
-export const logout = async () => {
+export const deleteEmployee = async (email) => {
   try {
-    await fetch("/api/Employees/logout", {
-      method: "POST",
+    const response = await fetch(`api/Employees/${email}`, {
+      method: "DELTE",
       headers: {
         "Content-Type": "application/json",
       },
     });
+    return response.ok;
   } catch (error) {
-    console.error("Failed to logout user: " + error);
+    console.error("User could not be deletet: " + error);
+    return false;
   }
 };
 
@@ -46,5 +82,58 @@ export const getMe = async () => {
   } catch (error) {
     console.error("Failed to get user data:", error);
     return null;
+  }
+};
+
+export const registerEmployee = async (firstName, lastName, password, role) => {
+  try {
+    const response = await fetch("api/Employees/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        firstName: firstName,
+        lastName: lastName,
+        password: password,
+        role: role,
+      }),
+    });
+    return response.ok;
+  } catch (error) {
+    console.error("User could not be created: " + error);
+    return false;
+  }
+};
+
+export const login = async (email, password) => {
+  try {
+    const response = await fetch("/api/Employees/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password,
+      }),
+    });
+    return response.ok;
+  } catch (error) {
+    console.error("User could not be logged in: " + error);
+    return false;
+  }
+};
+
+export const logout = async () => {
+  try {
+    await fetch("/api/Employees/logout", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (error) {
+    console.error("Failed to logout user: " + error);
   }
 };
