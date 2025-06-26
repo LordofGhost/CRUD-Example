95f0f46d30b822b1c89bdc1bc7c908457e26cf8a
Andreas Böhm <134922046+LordofGhost@users.noreply.github.com>
2025-06-26 07:43:20 +0000

    restructure App Routing to support auto navigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,34 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import { navigateToPage } from "./services/Navigation";
 import Administration from "./pages/Administration";
 import CashRegister from "./pages/CashRegister";
-import Login from "./pages/Login";
 import Warehouse from "./pages/Warehouse";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route />
-        <Route path="/administration" element={<Administration />} />
-        <Route path="/cashregister" element={<CashRegister />} />
-        <Route path="/warehouse" element={<Warehouse />} />
-        <Route path="*" element={<Login />} />
-      </Routes>
+      <AppContent />
     </Router>
+  );
+}
+
+function AppContent() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigateToPage(navigate);
+  }, [navigate]);
+
+  return (
+    <Routes>
+      <Route path="/administration" element={<Administration />} />
+      <Route path="/cashregister" element={<CashRegister />} />
+      <Route path="/warehouse" element={<Warehouse />} />
+      <Route path="*" element={<Login />} />
+    </Routes>
   );
 }
 
