4bbf639ef64e97f79fe8ac59a885410a8af804a4
Andreas Böhm <134922046+LordofGhost@users.noreply.github.com>
2025-06-28 12:08:02 +0000

    Add tailwind config, remove ide folder from git

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,13 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx,vue}"
-  ],
-  theme: {
-    extend: {
-      primary: '#202225',
-    },
-  },
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,vue}"],
   plugins: [],
-}
+};
