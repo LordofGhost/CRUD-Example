21194928dd6241c0ee53fc22c04661c4ccc4bc3a
Andreas Böhm <134922046+LordofGhost@users.noreply.github.com>
2025-06-28 12:19:07 +0000

    Add views to Administration page

--- frontend/src/pages/Administration.jsx
+++ frontend/src/pages/Administration.jsx
@@ -1,10 +1,38 @@
+import { useState } from "react";
 import Header from "../components/layout/Header";
+import Employees from "../components/view/Employees";
+import Products from "../components/view/Products";
+import Shelves from "../components/view/Shelves";
 
 function Administration() {
+  const [view, setView] = useState("Angestellte");
+
+  function handleViewChange(newView) {
+    setView(newView);
+  }
+
+  function renderContent() {
+    switch (view) {
+      case "Produkte":
+        return <Products />;
+      case "Regale":
+        return <Shelves />;
+      case "Angestellte":
+        return <Employees />;
+      default:
+        return <Employees />;
+    }
+  }
+
   return (
-    <div>
-      <Header PageName={"Verwaltung"} />
-    </div>
+    <>
+      <Header
+        Views={["Produkte", "Regale", "Angestellte"]}
+        StartView={"Angestellte"}
+        onActiveViewChange={handleViewChange}
+      />
+      <div>{renderContent()}</div>
+    </>
   );
 }
 
