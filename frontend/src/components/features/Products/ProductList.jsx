ed7476eadfb33d9ca3d0daa4c6030c3ed13856cf
Andreas Böhm <134922046+LordofGhost@users.noreply.github.com>
2025-06-29 22:14:58 +0000

    Create Product list

---
+++ frontend/src/components/features/Products/ProductList.jsx
@@ -0,0 +1,37 @@
+import { useCallback, useEffect, useState } from "react";
+import { getProducts } from "../../../services/Products";
+import ProductItem from "./ProductItem";
+
+function ProductList({ handleReload, categoryFilter }) {
+  const [productComponents, setProductComponents] = useState([
+    <div key={1}></div>,
+  ]);
+
+  const renderProducts = useCallback(async () => {
+    const products = await getProducts(categoryFilter);
+
+    if (products) {
+      const processedEmployees = products.map((product, index) => (
+        <ProductItem
+          Product={product}
+          key={`product-${index}`}
+          editable={true}
+          handleReload={handleReload}
+        />
+      ));
+      setProductComponents(processedEmployees);
+    }
+  }, [setProductComponents, handleReload, categoryFilter]);
+
+  useEffect(() => {
+    renderProducts();
+  }, [renderProducts]);
+
+  return (
+    <div className="mb-7 grid gap-10 md:grid-cols-2 lg:grid-cols-3">
+      {productComponents}
+    </div>
+  );
+}
+
+export default ProductList;
