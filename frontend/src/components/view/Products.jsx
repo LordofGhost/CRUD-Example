fc9ed21761ffd7898298eddee706a3952d271b9c
Andreas Böhm <134922046+LordofGhost@users.noreply.github.com>
2025-06-29 22:13:44 +0000

    Create Products view content

--- frontend/src/components/view/Products.jsx
+++ frontend/src/components/view/Products.jsx
@@ -1,9 +1,62 @@
+import { useState } from "react";
+import Button from "../common/Button";
 import CenterContainer from "../common/CenterContainer";
+import Dropdown from "../common/Dropdown";
+import InputTitle from "../common/InputTitle";
+import ProductList from "../features/Products/ProductList";
+import EditProduct from "../features/Products/EditProduct";
 
 function Products() {
+  const categorys = [
+    "Laptops",
+    "PCs",
+    "Tablets",
+    "Monitore",
+    "Drucker",
+    "Scanner",
+    "Speichermedien",
+    "Tastaturen",
+    "Mäuse",
+    "Zubehör",
+    "Alle",
+  ];
+  const [categoryFilter, setCategoryFilter] = useState("Alle");
+  const [reloadKey, setReloadKey] = useState(0);
+  const [isCreateMenuOpen, setIsCreateMenuOpen] = useState(false);
+
+  const handleReload = () => {
+    setReloadKey((prev) => prev + 1);
+  };
+
   return (
     <CenterContainer>
-      <div>Produkte</div>
+      <div className="mt-7 flex flex-row items-start justify-between">
+        <div className="flex flex-col">
+          <InputTitle Title={"Kategorie"} />
+          <Dropdown
+            options={categorys}
+            value={categoryFilter}
+            onChange={(e) => setCategoryFilter(e.target.value)}
+          />
+        </div>
+        <Button
+          style={"accept"}
+          text={"Produkt erstellen"}
+          onClick={() => setIsCreateMenuOpen(true)}
+        />
+        {isCreateMenuOpen && (
+          <EditProduct
+            handleReload={handleReload}
+            Product={null}
+            onClose={() => setIsCreateMenuOpen(false)}
+          />
+        )}
+      </div>
+      <ProductList
+        key={reloadKey}
+        handleReload={handleReload}
+        categoryFilter={categoryFilter}
+      />
     </CenterContainer>
   );
 }
