06845283127e7085456f3662f6b890dec025e86d
Andreas Böhm <134922046+LordofGhost@users.noreply.github.com>
2025-06-30 21:30:22 +0000

    Add buy Product service

---
+++ frontend/src/services/Stock.jsx
@@ -0,0 +1,18 @@
+export const restockProduct = async (productId, amount) => {
+  try {
+    const response = await fetch(`api/Stock/RestockProduct`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        productId: productId,
+        amount: amount,
+      }),
+    });
+    return response.ok;
+  } catch (error) {
+    console.error("Product could not be restocked: " + error);
+    return false;
+  }
+};
