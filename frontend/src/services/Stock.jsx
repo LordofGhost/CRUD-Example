0c660c39eacfae6f8363d620d246e86003c3be54
Andreas Böhm <134922046+LordofGhost@users.noreply.github.com>
2025-07-01 16:42:05 +0000

    Create sellProduct service

--- frontend/src/services/Stock.jsx
+++ frontend/src/services/Stock.jsx
@@ -16,3 +16,19 @@ export const restockProduct = async (productId, amount) => {
     return false;
   }
 };
+
+export const sellProduct = async (products) => {
+  try {
+    const response = await fetch(`api/Stock/SellProducts`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(products),
+    });
+    return response.ok;
+  } catch (error) {
+    console.error("Product could not be sold: " + error);
+    return false;
+  }
+};
