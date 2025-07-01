10a9af3735b3c797632f36fdbfb3ae5e79d35edd
Andreas Böhm <134922046+LordofGhost@users.noreply.github.com>
2025-07-01 13:36:39 +0000

    Update Context to refactor

--- backend/src/Data/Context/ShopDbContext.cs
+++ backend/src/Data/Context/ShopDbContext.cs
@@ -5,23 +5,7 @@ public class ShopDbContext : DbContext
 {
     public ShopDbContext(DbContextOptions<ShopDbContext> options) : base(options) { }
 
-    public DbSet<Models.Product> Products { get; set; }
-    public DbSet<Models.Shelf> Shelves { get; set; }
-    public DbSet<Models.Stock> Stock { get; set; }
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        modelBuilder.Entity<Models.Product>()
-            .HasOne(p => p.Shelf)
-            .WithMany(s => s.Products)
-            .HasForeignKey(p => p.ShelfId);
-
-        modelBuilder.Entity<Models.Shelf>()
-            .HasMany(s => s.Products)
-            .WithOne(p => p.Shelf)
-            .HasForeignKey(p => p.ShelfId)
-            .OnDelete(DeleteBehavior.SetNull);
-
-        base.OnModelCreating(modelBuilder);
-    }
+    public DbSet<Models.Products.Product> Products { get; set; }
+    public DbSet<Models.Shelves.Shelf> Shelves { get; set; }
+    public DbSet<Models.Stock.Stock> Stock { get; set; }
 }
--- backend/src/Data/Context/UserDbContext.cs
+++ backend/src/Data/Context/UserDbContext.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 namespace Jupiter.Data.Context;
 
-public class UserDbContext : IdentityDbContext<Models.Employee>
+public class UserDbContext : IdentityDbContext<Models.Employees.Employee>
 {
     public UserDbContext(DbContextOptions<UserDbContext> options) : base(options) { }
 }
