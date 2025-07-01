b558a13ea15a328cd4ce52b9abffdcd675499943
Andreas Böhm <134922046+LordofGhost@users.noreply.github.com>
2025-07-01 19:37:22 +0000

    Create Warehouse view

--- frontend/src/components/view/Warehouse.jsx
+++ frontend/src/components/view/Warehouse.jsx
@@ -1,9 +1,24 @@
+import { useState } from "react";
 import CenterContainer from "../common/CenterContainer";
+import TaskList from "../features/Warehouse/TaskList";
 
 function Warehouse() {
+  const [reloadKey, setReloadKey] = useState(0);
+
+  const handleReload = () => {
+    setReloadKey((prev) => prev + 1);
+  };
+
   return (
     <CenterContainer>
-      <div>Lager</div>
+      <div className="flex flex-col gap-2">
+        <div className="mt-7 text-center text-xl font-semibold">Aufgaben</div>
+        <div className="text-center">
+          Die Aufgelisteten Produkte müssen in die jeweiligen Regale eingeräumt
+          werden.
+        </div>
+      </div>
+      <TaskList key={reloadKey} handleReload={handleReload} />
     </CenterContainer>
   );
 }
