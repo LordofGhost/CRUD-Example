7baeefc2b937320fccfb42ff4bdbec422320e546
Andreas Böhm <134922046+LordofGhost@users.noreply.github.com>
2025-07-01 19:39:41 +0000

    Create Task functionality in StockController

--- backend/src/Controllers/StockController.cs
+++ backend/src/Controllers/StockController.cs
@@ -1,3 +1,4 @@
+using Jupiter.Models.Stock;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
@@ -15,22 +16,57 @@ public class StockController : ControllerBase
         _context = context;
     }
 
-    /*
-        [HttpGet("ShelfFillerTasks")]
-        [Authorize]
-        public async Task<Models.ShelfFillerTask> GetShelfFillerTasks()
+    [HttpGet("GetTasks")]
+    [Authorize]
+    public async Task<ActionResult<ICollection<Models.Stock.STask>>> GetTasks()
+    {
+        ICollection<STask> tasks = [];
+        var stocks = await _context.Stock.ToListAsync();
+
+        foreach (var stock in stocks)
         {
-            // TODO
+            // Skip entry if newer exists
+            if (await NewerStockExists(stock.ProductId, stock.Day)) continue;
+
+            if (stock.OnTheShelf < await GetCompartmentsSizeOfProduct(stock.ProductId) && stock.InStock > 0)
+            {
+                var product = await _context.Products.FindAsync(stock.ProductId);
+
+                if (product == null) return NotFound("The product of the stock could not be found.");
+                // If no shelf is set, it can not be filled
+                if (product.ShelfId == null) continue;
+
+                tasks.Add(new STask
+                {
+                    ProductId = stock.ProductId,
+                    ProductName = product.Name,
+                    Amount = await GetCompartmentsSizeOfProduct(stock.ProductId) - stock.OnTheShelf,
+                    ShelfId = (int)product.ShelfId
+                });
+            }
         }
 
-        [HttpPatch("ShelfFillerTaskComplete")]
-        [Authorize]
-        public async Task<IActionResult> SetShelfFillerTaskComplete()
+        return CreatedAtAction(nameof(GetTasks), tasks);
+    }
+
+    [HttpPatch("TaskComplete")]
+    [Authorize]
+    public async Task<IActionResult> TaskComplete(Models.Stock.SProduct product)
+    {
+        Models.Stock.Stock stock = await GetOrCreateTodayEntry(product.ProductId);
+
+        if (stock.InStock < product.Amount || stock.OnTheShelf + product.Amount > await GetCompartmentsSizeOfProduct(product.ProductId))
         {
-            // TODO
-            return NoContent();
+            return BadRequest("Not enough Products in stock or to many on the shelf.");
         }
-    **/
+
+        stock.InStock -= product.Amount;
+        stock.OnTheShelf += product.Amount;
+
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
 
     [HttpPatch("SellProducts")]
     [Authorize]
@@ -82,6 +118,11 @@ public class StockController : ControllerBase
 
     private async Task<Models.Stock.Stock> CreateTodaysEntry(int ProductId)
     {
+        var product = await _context.Products.FindAsync(ProductId);
+        if (product == null)
+        {
+            throw new Exception($"Product with ID {ProductId} does not exist.");
+        }
         Models.Stock.Stock? oldEntry = await GetLatestEntry(ProductId);
         return new Models.Stock.Stock
         {
@@ -95,13 +136,43 @@ public class StockController : ControllerBase
 
     private async Task<Models.Stock.Stock> GetOrCreateTodayEntry(int ProductId)
     {
-            if (!TodaysEntryExists(ProductId))
+        if (!TodaysEntryExists(ProductId))
+        {
+            var newEntry = await CreateTodaysEntry(ProductId);
+            var entity = _context.Add(newEntry).Entity;
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+        else
+        {
+            return _context.Stock.First(e => e.ProductId == ProductId && e.Day == DateTime.Today);
+        }
+    }
+
+    private async Task<int> GetCompartmentsSizeOfProduct(int ProductId)
+    {
+        var product = await _context.Products.FindAsync(ProductId);
+
+        if (product == null || product.ShelfId == null) return 0;
+
+        var shelf = await _context.Shelves.FindAsync(product.ShelfId);
+
+        if (shelf == null) return 0;
+
+        return shelf.CompartmentsSize;
+    }
+
+    private async Task<bool> NewerStockExists(int ProductId, DateTime Day)
+    {
+        var stocks = await _context.Stock.ToListAsync();
+
+        foreach (var stock in stocks)
+        {
+            if (stock.ProductId == ProductId && stock.Day > Day)
             {
-                return _context.Add(await CreateTodaysEntry(ProductId)).Entity;
+                return true;
             }
-            else
-            {
-                return _context.Stock.First(e => e.ProductId == ProductId && e.Day == DateTime.Today);
-            }
+        }
+        return false;
     }
 }
