46cc344d35cadd1542cb4526617d5d13e0ae26a2
Andreas Böhm <134922046+LordofGhost@users.noreply.github.com>
2025-07-01 13:28:24 +0000

    Create Shelves view

--- frontend/src/components/view/Shelves.jsx
+++ frontend/src/components/view/Shelves.jsx
@@ -1,9 +1,33 @@
+import { useState } from "react";
+import Button from "../common/Button";
 import CenterContainer from "../common/CenterContainer";
+import CreateShelf from "../features/Shelves/CreateShelf";
+import ShelfList from "../features/Shelves/ShelfList";
 
 function Shelves() {
+  const [reloadKey, setReloadKey] = useState(0);
+  const [isCreateMenuOpen, setIsCreateMenuOpen] = useState(false);
+
+  const handleReload = () => {
+    setReloadKey((prev) => prev + 1);
+  };
+
   return (
     <CenterContainer>
-      <div>Regale</div>
+      <div className="mt-7 flex flex-row justify-end">
+        <Button
+          style={"accept"}
+          text={"Regal erstellen"}
+          onClick={() => setIsCreateMenuOpen(true)}
+        />
+        {isCreateMenuOpen && (
+          <CreateShelf
+            onClose={() => setIsCreateMenuOpen(false)}
+            handleReload={handleReload}
+          />
+        )}
+      </div>
+      <ShelfList key={reloadKey} handleReload={handleReload} />
     </CenterContainer>
   );
 }
