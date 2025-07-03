324cc612aba80e93f33610b623a612e47400036b
Andreas Böhm <134922046+LordofGhost@users.noreply.github.com>
2025-07-03 06:44:27 +0000

    Create ProductList for CashRegister

---
+++ frontend/src/components/features/CashRegister/ProductList.jsx
@@ -0,0 +1,38 @@
+import { useCallback, useEffect, useState } from "react";
+import { getProducts } from "../../../services/Products";
+import ProductItem from "../Products/ProductItem";
+
+function ProductList({ categoryFilter, onClick }) {
+  const [productComponents, setProductComponents] = useState([
+    <div key={1}></div>,
+  ]);
+
+  const renderProducts = useCallback(async () => {
+    const products = await getProducts(categoryFilter);
+
+    if (products) {
+      const processedEmployees = products.map((product, index) => (
+        <ProductItem
+          Product={product}
+          key={`product-${index}`}
+          editable={false}
+          showStock={false}
+          onClick={() => onClick(product.productId)}
+        />
+      ));
+      setProductComponents(processedEmployees);
+    }
+  }, [setProductComponents, categoryFilter]);
+
+  useEffect(() => {
+    renderProducts();
+  }, [renderProducts]);
+
+  return (
+    <div className="grid grid-cols-1 gap-10 overflow-hidden p-7 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3">
+      {productComponents}
+    </div>
+  );
+}
+
+export default ProductList;
