6701fd71fc735eaa03607bfacd70764568640988
Andreas Böhm <134922046+LordofGhost@users.noreply.github.com>
2025-07-03 06:45:36 +0000

    Refactor CashRegister

--- frontend/src/components/view/CashRegister.jsx
+++ frontend/src/components/view/CashRegister.jsx
@@ -1,14 +1,19 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import ProductList from "../features/CashRegister/ProductList";
 import { sellProduct } from "../../services/Stock";
 import Button from "../common/Button";
 import Warning from "../common/Warning";
-import SellList from "../features/CashRegister/SellList";
+import { getProduct } from "../../services/Products";
 
 function CashRegister() {
   const [shoppingCart, setShoppingCart] = useState([]);
   const [showWarning, setShowWarning] = useState(false);
+  const [totalPrice, setTotalPrice] = useState(0);
+  const [productComponents, setProductComponents] = useState([
+    <div key={1}></div>,
+  ]);
 
+  // Scanner
   useEffect(() => {
     let scannedCode = "";
 
@@ -31,9 +36,22 @@ function CashRegister() {
     };
   }, []);
 
+  // Get total price
+  useEffect(() => {
+    var totalProceTemp;
+
+    shoppingCart.forEach((product) => {
+      totalProceTemp += product.sellingPrice;
+    });
+
+    setTotalPrice(totalProceTemp);
+  }, [shoppingCart, setTotalPrice]);
+
   function handleProductSelect(productId) {
-    setShoppingCart((prev) => {
-      const existingProduct = prev.find((item) => item.productId === productId);
+    setShoppingCart(async (prev) => {
+      const existingProduct = prev.find(
+        (product) => product.productId === productId,
+      );
       if (existingProduct) {
         return prev.map((item) =>
           item.productId === productId
@@ -41,22 +59,27 @@ function CashRegister() {
             : item,
         );
       } else {
-        return [...prev, { productId: productId, amount: 1 }];
+        const newProduct = await getProduct(productId);
+        return [...prev, { ...newProduct, amount: 1 }];
       }
     });
   }
 
-  function handleCountChange(productId, amount) {
-    setShoppingCart((prev) => {
-      return prev.map((item) =>
-        item.productId === productId ? { ...item, amount: amount } : item,
-      );
-    });
+  function handleAmountChange(product, amount) {
+    if (amount > 1 && amount <= product.onTheShelf) {
+      setShoppingCart((prev) => {
+        return prev.map((item) =>
+          item.productId === product.productId
+            ? { ...item, amount: amount }
+            : item,
+        );
+      });
+    }
   }
 
-  function handleProductRemove(productId) {
+  function handleProductRemove(product) {
     setShoppingCart((prev) =>
-      prev.filter((item) => item.productId !== productId),
+      prev.filter((item) => item.productId !== product.productId),
     );
   }
 
@@ -73,6 +96,26 @@ function CashRegister() {
   function handleCancel() {
     setShoppingCart([]);
   }
+
+  const renderProducts = useCallback(async () => {
+    if (shoppingCart.length > 0) {
+      const processedProducts = await shoppingCart.map(
+        async (product, index) => (
+          <SellItem
+            key={`sellProduct-${index}`}
+            product={product}
+            handleAmountChange={handleAmountChange}
+            handleProductRemove={handleProductRemove}
+          />
+        ),
+      );
+      setProductComponents(processedProducts);
+    }
+  }, [shoppingCart, handleAmountChange, handleProductRemove]);
+
+  useEffect(() => {
+    renderProducts();
+  }, [renderProducts]);
 
   return (
     <div className="flex h-[calc(100vh-4rem)] overflow-hidden">
@@ -91,22 +134,23 @@ function CashRegister() {
             />
           )}
           <div className="flex flex-1 flex-col">
-            <SellList
-              products={shoppingCart}
-              handleCountChange={handleCountChange}
-              handleProductRemove={handleProductRemove}
-            />
-            <div className="flex w-full justify-end gap-2">
-              <Button
-                style={"cancel"}
-                onClick={handleCancel}
-                text={"Zurücksetzen"}
-              />
-              <Button
-                style={"accept"}
-                onClick={handleSell}
-                text={"Verkaufen"}
-              />
+            <div className="mb-7 flex flex-1 flex-col justify-end gap-4">
+              {productComponents}
+            </div>
+            <div className="flex w-full justify-between">
+              <div className="items-center">{totalPrice}€</div>
+              <div className="flex gap-2">
+                <Button
+                  style={"cancel"}
+                  onClick={handleCancel}
+                  text={"Zurücksetzen"}
+                />
+                <Button
+                  style={"accept"}
+                  onClick={handleSell}
+                  text={"Verkaufen"}
+                />
+              </div>
             </div>
           </div>
         </div>
